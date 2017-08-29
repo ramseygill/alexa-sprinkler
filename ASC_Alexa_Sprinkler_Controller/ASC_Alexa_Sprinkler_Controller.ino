6a59cc5040017140204d21ac4d0a8c57ad73614d
ramseygill <ramseygill@gmail.com>
2017-08-28 20:26:14 -0700

    Added MCP23017 I/O expander

--- ASC_Alexa_Sprinkler_Controller/ASC_Alexa_Sprinkler_Controller.ino
+++ ASC_Alexa_Sprinkler_Controller/ASC_Alexa_Sprinkler_Controller.ino
@@ -7,8 +7,11 @@
  * Homepage: https://hackaday.io/project/26850-alexa-enabled-sprinkler-controller
  * GitHub: https://github.com/ramseygill/alexa-sprinkler
  * ========================================================================================
- * NOTES
+ * NOTES, Credit, Sources, and Props
  * http://www.geekstips.com/arduino-time-sync-ntp-server-esp8266-udp/
+ * Adafruit-MCP23017-Arduino-Library
+ * Copyright (c) 2012, Adafruit Industries All rights reserved.
+ * https://github.com/adafruit/Adafruit-MCP23017-Arduino-Library/blob/master/license.txt
  */
  //LIBRARIES-------------------------------------------------------------------------------
 #include <ESP8266WiFi.h>
@@ -16,8 +19,10 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 #include "SSD1306.h" // SSD1306
+#include "Adafruit_MCP23017.h" //Adafruit-MCP23017-Arduino-Library
 
  //CONSTANTS/VARIABLES/GLOBALS-------------------------------------------------------------
+Adafruit_MCP23017 mcp; //Adafruit-MCP23017-Arduino-Library
 SSD1306  display(0x3c, D3, D5); // SSD1306 Pin Assignments
 MDNSResponder mdns;
 
@@ -27,44 +32,40 @@ const char* ssid     = "skynet-2GHz";     //wifi SSID
 const char* password = "haxor1337";       //wifi password
 
 //PIN DEFINITIONS
-const int k1Pin = D0;  //ESP-12E D1 RELAYS 1-8
-const int k2Pin = D1;  //ESP-12E D2
-const int k3Pin = D2;  //ESP-12E D4
-const int k4Pin = D4; //ESP-12E D6
-const int k5Pin = D6; //ESP-12E D7
-const int k6Pin = D7; //ESP-12E D8
-//const int k7Pin = 0;  //ESP-12E D3
-//const int k8Pin = 14; //ESP-12E D5
-//Wire SDA on OLED to D3 (GPIO0) pin on ESP-12E (used for SSD1306 OLED)
-//Wire SCL on OLED to D5 (GPIO14) pin on ESP-12E (used for SSD1306 OLED)
 
+//VARIABLES
 int relayState = 0;
 
  //SETUP-----------------------------------------------------------------------------------
 void setup() {
 
+  mcp.begin();      // use default address 0 Adafruit-MCP23017-Arduino-Library
+  
   display.init(); //SSD12306 init
   
   display.flipScreenVertically(); //SSD12306
   display.setFont(ArialMT_Plain_10); //SSD12306 set font
-  
-  pinMode(k1Pin, OUTPUT);  // set relay pins as outputs
-  pinMode(k2Pin, OUTPUT);  //
-  pinMode(k3Pin, OUTPUT);  //
-  pinMode(k4Pin, OUTPUT);  //
-  pinMode(k5Pin, OUTPUT);  //
-  pinMode(k6Pin, OUTPUT);  //
-  //pinMode(k7Pin, OUTPUT);  //
-  //pinMode(k8Pin, OUTPUT);  //
 
-  digitalWrite(k1Pin, HIGH); // ensure that each pin defaults to HIGH (relay off)
-  digitalWrite(k2Pin, HIGH); // these cheap blue relay modules use inverted logic 
-  digitalWrite(k3Pin, HIGH);
-  digitalWrite(k4Pin, HIGH);
-  digitalWrite(k5Pin, HIGH);
-  digitalWrite(k6Pin, HIGH);
-  //digitalWrite(k7Pin, HIGH);
-  //digitalWrite(k8Pin, HIGH);
+//set PORT A I/O expander pins to OUTPUTS
+  mcp.pinMode(0, OUTPUT); //MCP23017 PIN 21 GPA0 (K1 RELAY)
+  mcp.pinMode(1, OUTPUT); //MCP23017 PIN 22 GPA1 (K2 RELAY)
+  mcp.pinMode(2, OUTPUT);
+  mcp.pinMode(3, OUTPUT);
+  mcp.pinMode(4, OUTPUT);
+  mcp.pinMode(5, OUTPUT);
+  mcp.pinMode(6, OUTPUT);
+  mcp.pinMode(7, OUTPUT);
+
+// ensure that each pin defaults to HIGH (relay off)
+// these cheap blue relay modules use active low logic.
+  mcp.digitalWrite(0, HIGH); //K1
+  mcp.digitalWrite(1, HIGH); //K2
+  mcp.digitalWrite(2, HIGH); //K3
+  mcp.digitalWrite(3, HIGH); //K4
+  mcp.digitalWrite(4, HIGH); //K5
+  mcp.digitalWrite(5, HIGH); //K6
+  mcp.digitalWrite(6, HIGH); //K7
+  mcp.digitalWrite(7, HIGH); //K8
 
 //Webpage Heading
   webPage += "<h1>ASC - Alexa Sprinkler Controller</h1>";
@@ -96,7 +97,6 @@ void setup() {
   webPage += "<p>Relay #6 ";
   webPage += "<a href=\"relay6On\"><button>ON</button></a>&nbsp;";
   webPage += "<a href=\"relay6Off\"><button>OFF</button></a></p>";
-/*
 //Relay 7 button objects
   webPage += "<p>Relay #7 ";
   webPage += "<a href=\"relay7On\"><button>ON</button></a>&nbsp;";
@@ -105,7 +105,7 @@ void setup() {
   webPage += "<p>Relay #8 ";
   webPage += "<a href=\"relay8On\"><button>ON</button></a>&nbsp;";
   webPage += "<a href=\"relay8Off\"><button>OFF</button></a></p>";
- */
+
   Serial.begin(115200); //start UART seral communication at 115200 baud
   delay(100);
  
@@ -136,14 +136,14 @@ void setup() {
   server.on("/relay1On", [](){
     server.send(200, "text/html", webPage);
     // Turn on RELAY
-    digitalWrite(k1Pin, LOW);
+    mcp.digitalWrite(0, LOW);
     Serial.println("RELAY 1 ON");
     delay(500);
   });
   server.on("/relay1Off", [](){
     server.send(200, "text/html", webPage);
     //Turn off RELAY
-    digitalWrite(k1Pin, HIGH);
+    mcp.digitalWrite(0, HIGH);;
     Serial.println("RELAY 1 OFF");
     delay(500); 
   });
@@ -151,14 +151,14 @@ void setup() {
     server.on("/relay2On", [](){
     server.send(200, "text/html", webPage);
     // Turn on RELAY
-    digitalWrite(k2Pin, LOW);
+    mcp.digitalWrite(1, LOW);
     Serial.println("RELAY 2 ON");
     delay(500);
   });
   server.on("/relay2Off", [](){
     server.send(200, "text/html", webPage);
     //Turn off RELAY
-    digitalWrite(k2Pin, HIGH);
+    mcp.digitalWrite(1, HIGH);
     Serial.println("RELAY 2 OFF");
     delay(500); 
   });
@@ -166,14 +166,14 @@ void setup() {
     server.on("/relay3On", [](){
     server.send(200, "text/html", webPage);
     // Turn on RELAY
-    digitalWrite(k3Pin, LOW);
+    mcp.digitalWrite(2, LOW);
     Serial.println("RELAY 3 ON");
     delay(500);
   });
   server.on("/relay3Off", [](){
     server.send(200, "text/html", webPage);
     //Turn off RELAY
-    digitalWrite(k3Pin, HIGH);
+    mcp.digitalWrite(2, HIGH);
     Serial.println("RELAY 3 OFF");
     delay(500); 
   });
@@ -181,14 +181,14 @@ void setup() {
     server.on("/relay4On", [](){
     server.send(200, "text/html", webPage);
     // Turn on RELAY
-    digitalWrite(k4Pin, LOW);
+    mcp.digitalWrite(3, LOW);
     Serial.println("RELAY 4 ON");
     delay(500);
   });
   server.on("/relay4Off", [](){
     server.send(200, "text/html", webPage);
     //Turn off RELAY
-    digitalWrite(k4Pin, HIGH);
+    mcp.digitalWrite(3, HIGH);
     Serial.println("RELAY 4 OFF");
     delay(500); 
   });
@@ -196,14 +196,14 @@ void setup() {
     server.on("/relay5On", [](){
     server.send(200, "text/html", webPage);
     // Turn on RELAY
-    digitalWrite(k5Pin, LOW);
+    mcp.digitalWrite(4, LOW);
     Serial.println("RELAY 5 ON");
     delay(500);
   });
   server.on("/relay5Off", [](){
     server.send(200, "text/html", webPage);
     //Turn off RELAY
-    digitalWrite(k5Pin, HIGH);
+    mcp.digitalWrite(4, HIGH);
     Serial.println("RELAY 5 OFF");
     delay(500); 
   });
@@ -211,30 +211,29 @@ void setup() {
     server.on("/relay6On", [](){
     server.send(200, "text/html", webPage);
     // Turn on RELAY
-    digitalWrite(k6Pin, LOW);
+    mcp.digitalWrite(5, LOW);
     Serial.println("RELAY 6 ON");
     delay(500);
   });
   server.on("/relay6Off", [](){
     server.send(200, "text/html", webPage);
     //Turn off RELAY
-    digitalWrite(k6Pin, HIGH);
+    mcp.digitalWrite(5, HIGH);
     Serial.println("RELAY 6 OFF");
     delay(500); 
   });
-   /*
     //RELAY 7 ------------------------------------
     server.on("/relay7On", [](){
     server.send(200, "text/html", webPage);
     // Turn on RELAY
-    //digitalWrite(k7Pin, LOW);
+    mcp.digitalWrite(6, LOW);
     Serial.println("RELAY 7 ON");
     delay(500);
   });
   server.on("/relay7Off", [](){
     server.send(200, "text/html", webPage);
     //Turn off RELAY
-    //digitalWrite(k7Pin, HIGH);
+    mcp.digitalWrite(6, HIGH);
     Serial.println("RELAY 7 OFF");
     delay(500); 
   });
@@ -242,47 +241,50 @@ void setup() {
     server.on("/relay8On", [](){
     server.send(200, "text/html", webPage);
     // Turn on RELAY
-    //digitalWrite(k8Pin, LOW);
+    mcp.digitalWrite(7, LOW);
     Serial.println("RELAY 8 ON");
     delay(500);
   });
   server.on("/relay8Off", [](){
     server.send(200, "text/html", webPage);
     //Turn off RELAY
-    //digitalWrite(k8Pin, HIGH);
+    mcp.digitalWrite(7, HIGH);
     Serial.println("RELAY 8 OFF");
     delay(500); 
   });
-  */
+  
       //ALL RELAYS ------------------------------------
     server.on("/allOn", [](){ //ON
     server.send(200, "text/html", webPage);
     // Turn on RELAY
-      digitalWrite(k1Pin, LOW);
-      digitalWrite(k2Pin, LOW);
-      digitalWrite(k3Pin, LOW);
-      digitalWrite(k4Pin, LOW);
-      digitalWrite(k5Pin, LOW);
-      digitalWrite(k6Pin, LOW);
-      //digitalWrite(k7Pin, LOW);
-      //digitalWrite(k8Pin, LOW);
+    mcp.digitalWrite(0, LOW); //K1
+    mcp.digitalWrite(1, LOW);
+    mcp.digitalWrite(2, LOW);
+    mcp.digitalWrite(3, LOW);
+    mcp.digitalWrite(4, LOW);
+    mcp.digitalWrite(5, LOW);
+    mcp.digitalWrite(6, LOW);
+    mcp.digitalWrite(7, LOW);
+    
       Serial.println("ALL ON");
 
       relayState = 1; //set state for screen update
       
     delay(100);
   });
+  
   server.on("/allOff", [](){ //OFF
     server.send(200, "text/html", webPage);
     //Turn off RELAY
-      digitalWrite(k1Pin, HIGH);
-      digitalWrite(k2Pin, HIGH);
-      digitalWrite(k3Pin, HIGH);
-      digitalWrite(k4Pin, HIGH);
-      digitalWrite(k5Pin, HIGH);
-      digitalWrite(k6Pin, HIGH);
-      //digitalWrite(k7Pin, HIGH);
-      //digitalWrite(k8Pin, HIGH);
+    mcp.digitalWrite(0, HIGH);
+    mcp.digitalWrite(1, HIGH);
+    mcp.digitalWrite(2, HIGH);
+    mcp.digitalWrite(3, HIGH);
+    mcp.digitalWrite(4, HIGH);
+    mcp.digitalWrite(5, HIGH);
+    mcp.digitalWrite(6, HIGH);
+    mcp.digitalWrite(7, HIGH);
+    
       Serial.println("ALL OFF");
 
       relayState = 0; //set state for screen update
